a335ba6b9aa63c2a74cc7cc30cfe583775f426c1
sudotx <64108848+sudotx@users.noreply.github.com>
2024-07-14 16:52:58 +0100

    cleaup

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import 'reflect-metadata'
 import 'source-map-support/register'
 
 import sendHelp from '@/handlers/help'
-import handleLanguage from '@/handlers/language'
 import bot from '@/helpers/bot'
 import i18n from '@/helpers/i18n'
 import startMongo from '@/helpers/startMongo'
@@ -11,8 +10,23 @@ import languageMenu from '@/menus/language'
 import attachUser from '@/middlewares/attachUser'
 import configureI18n from '@/middlewares/configureI18n'
 import { run } from '@grammyjs/runner'
-import { ignoreOld, sequentialize } from 'grammy-middlewares'
 import axios from 'axios'
+import { InlineKeyboard } from 'grammy'
+import { ignoreOld, sequentialize } from 'grammy-middlewares'
+
+const mainMenu = new InlineKeyboard()
+  .text('Get Neko Image', 'get_neko')
+  .row()
+  .text('Download Manga', 'download_manga')
+  .row()
+  .text('Trigger Something', 'download_manga')
+
+// show the main menu
+bot.command('menu', (ctx) => {
+  ctx.reply('Welcome! Please choose an option:', {
+    reply_markup: mainMenu
+  });
+});
 
 async function runApp() {
   console.log('Starting app...')
@@ -30,29 +44,33 @@ async function runApp() {
     .use(languageMenu)
   // Commands
   bot.command(['help', 'start'], sendHelp)
-  bot.command('language', handleLanguage)
-  bot.command('earn', () => { console.log("Earn Clicked") })
-  bot.command('download', async (ctx) => {
-    const mangaName = ctx.message?.text?.split(' ').slice(1).join(' ');
-    if (!mangaName) {
-      ctx.reply('Please provide a manga name.');
-      return;
-    }
+  // bot.command('language', handleLanguage)
+  bot.command('earn', async (ctx) => {
+    ctx.reply("Okay earning")
 
+  })
+
+  bot.callbackQuery('get_neko', async (ctx) => {
     try {
-      const mangaData = await fetchManga(mangaName);
-      if (mangaData.data.length === 0) {
-        ctx.reply('Manga not found.');
-        return;
-      }
+      const imageUrl = await fetchNeko();
 
-      const manga = mangaData.data[0];
-      ctx.reply(`Found: ${manga.attributes.title.en}`);
-      // Add code to download and send manga files
+      await ctx.reply(imageUrl)
     } catch (error) {
-      ctx.reply('An error occurred while fetching manga.');
+      await ctx.reply('An error occurred while fetching the neko image.');
     }
+    await ctx.answerCallbackQuery();
   });
+  bot.callbackQuery('get_nsfw_neko', async (ctx) => {
+    try {
+      const imageUrl = await getNsfwNeko();
+
+      await ctx.reply(imageUrl)
+    } catch (error) {
+      await ctx.reply('An error occurred while fetching the neko image.');
+    }
+    await ctx.answerCallbackQuery();
+  });
+
   // Errors
   bot.catch(console.error)
   // Start bot
@@ -62,9 +80,33 @@ async function runApp() {
 }
 
 void runApp()
-const fetchManga = async (mangaName: string) => {
-  const response = await axios.get(`https://api.mangadex.org/manga?title=${mangaName}`);
-  console.log(response);
-  return response.data;
+
+const fetchNeko = async (): Promise<string> => {
+  try {
+    const response = await axios.get('https://nekos.pro/api/neko', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data.url;
+  } catch (error) {
+    console.error('Error fetching neko image:', error);
+    throw error;
+  }
+
+};
+const getNsfwNeko = async (): Promise<string> => {
+  try {
+    const response = await axios.get('https://nekos.pro/api/neko', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data.url;
+  } catch (error) {
+    console.error('Error fetching neko image:', error);
+    throw error;
+  }
+
 };
 
